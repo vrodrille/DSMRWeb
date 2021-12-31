938c97f5d4c68b210c9d2df4c67720b4146ac759
Vicente Rodríguez Lledó <vicenter8@gmail.com>
2021-12-31 18:47:16 +0100

    Corrected markup and style of form with new Bootstrap classes

--- app/javascript/components/CreateUpdateModal/index.js
+++ app/javascript/components/CreateUpdateModal/index.js
@@ -55,42 +55,34 @@ function CreateUpdateModal({ latitudeLongitude, onClose, addSensorAndCloseModal 
           </div>
           <form onSubmit={handleSubmit}>
             <div className="modal-body">
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label">Latitud:</label>
-                </div>
+              <div className="row mb-3">
+                <label className="col-3 col-form-label">Latitud:</label>
                 <div className="col-auto">
                   <input className="form-control" placeholder={latitudeLongitude ? latitudeLongitude.lat : null} disabled/>
                 </div>
               </div>
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label">Longitud:</label>
-                </div>
+              <div className="row mb-3">
+                <label className="col-3 col-form-label">Longitud:</label>
                 <div className="col-auto">
                   <input className="form-control" placeholder={latitudeLongitude ? latitudeLongitude.lng: null} disabled/>
                 </div>
               </div>
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label" htmlFor="inputLocation">Localización:</label>
-                </div>
+              <div className="row mb-3">
+                <label className="col-3 col-form-label" htmlFor="inputLocation">Localización:</label>
                 <div className="col-auto">
                   <input className={errors.location ? "form-control is-invalid" : "form-control"} id="inputLocation" name="location" onChange={handleChange}/>
                   <InputError errorMessage={errors.location ? errors.location[0] : null}/>
                 </div>
               </div>
-              <div className="row g-3 align-items-center">
-                <div className="col-auto">
-                  <label className="col-form-label" htmlFor="inputIP">Dirección IP:</label>
-                </div>
+              <div className="row">
+                <label className="col-3 col-form-label" htmlFor="inputIP">Dirección IP:</label>
                 <div className="col-auto">
                   <input className={errors.ip_address ? "form-control is-invalid" : "form-control"} id="inputIP" name="ip_address" onChange={handleChange}/>
                   <InputError errorMessage={errors.ip_address ? errors.ip_address[0] : null}/>
                 </div> 
               </div>
             </div>
-            <hr />
+            <hr className="mt-0"/>
             <h6 className="info-section-header"> Información </h6>
             <hr />
             <div className="input-group">
--- app/javascript/components/InputError/index.js
+++ app/javascript/components/InputError/index.js
@@ -2,11 +2,8 @@ import React from 'react'
 
 export default function InputError({ errorMessage }){
   return (
-    <>
-      { errorMessage ? <div className="invalid-feedback">
-          {errorMessage}
-        </div> : null
-      }
-    </>
+    <div className="invalid-feedback">
+      { errorMessage ? errorMessage: null }
+    </div> 
   )
 }
