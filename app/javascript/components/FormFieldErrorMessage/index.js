bbced2fc6968d1a53669ee307ca54d553d96ad9c
Vicente Rodríguez Lledó <vicenter8@gmail.com>
2022-06-02 19:00:21 +0200

    FormFieldErrorMessage documented

--- app/javascript/components/FormFieldErrorMessage/index.js
+++ app/javascript/components/FormFieldErrorMessage/index.js
@@ -1,5 +1,11 @@
 import React from 'react'
 
+/**
+ * Este componente se encarga de renderizar un mensaje de error.
+ * @param {String} errorMessage Parámetro que contiene el mensaje de error a renderizar.
+ * @param {String} tagClassName Parámetro que contiene la clase del div en el que se sitúa 
+ * el mensaje de error.
+ */
 export default function FormFieldErrorMessage({ errorMessage, tagClassName }){
   return (
     <div className={tagClassName}>
