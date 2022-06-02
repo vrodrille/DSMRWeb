004975effcdff2c0cc3a134987d5636f5cf197ad
Vicente Rodríguez Lledó <vicenter8@gmail.com>
2022-06-02 17:41:16 +0200

    Sidebar component documented

--- app/javascript/components/Sidebar/index.js
+++ app/javascript/components/Sidebar/index.js
@@ -3,11 +3,29 @@ import './Sidebar.css'
 import LaunchExperimentModal from '../LaunchExperimentModal'
 import ExperimentsResultsModal from '../ExperimentsResultsModal'
 
+/**
+ * Este componente representa la Sidebar de la página principal de la Aplicación, renderizando esta y su contenido, es decir,
+ * renderizando los botones que invocan a sus modales correspondientes. La muestra y cierre de estos modales (LaunchExperimentModal y
+ * ExperimentsResultsModal) en la Aplicación es tarea única y exclusiva de Bootstrap, esto es, no se ha implementado lógica alguna para
+ * su visualización y ocultamiento.
+ */
 function Sidebar(){
 
+  /**
+   * Este hook useState es empleado para almacenar el componente Modal de Bootstrap que se utilizará para controlar la visualización y 
+   * ocultamiento del componente LaunchExperimentModal.
+   */
   const [launchExperimentModal, setLaunchExperimentModal] = useState(null)
+  /**
+   * Este hook useState es empleado para almacenar el componente Modal de Bootstrap que se utilizará para controlar la visualización y
+   * ocultamiento del componente ExperimentsResultsModal.
+   */
   const [experimentsResultsModal, setExperimentsResultModal] = useState(null)
 
+  /**
+   * Este hook useEffect es utilizado para fijar como estado de launchExperimentModal y experimentsResultsModal el Modal de Bootstrap
+   * correspondiente. Su lista de dependencias es vacía, por lo cual se ejecutará únicamente en el primer renderizado.
+   */
   useEffect(() => {
     let launchExperimentModal = new bootstrap.Modal(document.getElementById('launch-experiment-modal'), {})
     setLaunchExperimentModal(launchExperimentModal)
